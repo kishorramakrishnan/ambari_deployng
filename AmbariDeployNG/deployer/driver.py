39c06a6ddc9fa21127c45f3a0c30b8275f9d0d30
Kishor Ramakrishnan <kramakrishnan@hortonworks.com>
2017-03-15 23:02:05 -0700

    More Fix

--- AmbariDeployNG/deployer/driver.py
+++ AmbariDeployNG/deployer/driver.py
@@ -336,7 +336,7 @@ def deploy():
     hosts_file = open("/root/hosts","r")
     all_hosts = hosts_file.read().splitlines()
     agent_hosts = all_hosts[0:len(all_hosts)-1]
-    prepare_configs(agent_hosts)
+    prepare_configs_magic(agent_hosts)
     ambari_host = agent_hosts[0]
     install_and_setup_mysql_connector()
     restart_ambari_server(ambari_host)
