4c6d6b0dc809de47c455d51bd15efccf61436b5f
Kishor Ramakrishnan <kramakrishnan@hortonworks.com>
2017-03-21 16:22:13 -0700

    Fix jdbc failure

--- configs_util.py
+++ configs_util.py
@@ -12,3 +12,9 @@ def setup_ranger_policy_url(policy_host,blueprint_file):
     logger.info("Updating Ranger Policy Manager URL in blueprint {0}".format(blueprint_file))
     logger.info("Policy Manager URL is {0}".format(policy_host))
     ssh_utils.run_shell_command("sed -i 's/POLICY_URL_PLACEHOLDER/{0}/g' {1}".format(policy_host, blueprint_file))
+
+def update_db_hosts_in_blueprint(db_host,blueprint_file):
+    logger.info("Updating DB HOSTS in blueprint {0}".format(blueprint_file))
+    logger.info("DB HOSTS is {0}".format(db_host))
+    ssh_utils.run_shell_command("sed -i 's/DB_HOST_PLACEHOLDER/{0}/g' {1}".format(db_host, blueprint_file))
+
