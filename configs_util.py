d8a298d4f17e33933bbeee1bcfd767601f6afeed
Kishor Ramakrishnan <kramakrishnan@hortonworks.com>
2017-04-04 16:32:35 -0700

    indent fix

--- configs_util.py
+++ configs_util.py
@@ -48,10 +48,10 @@ def replace_string_in_file(file_name,original_string,replacement_string):
     # Replace the file content
     with open(file_name, 'r+') as file:
         file_content = file.read()
-    file.seek(0)
-    file_content = file_content.replace(original_string, replacement_string)
-    file.write(file_content)
-    logger.info("after replacing hosts {0}".format(file_content))
+        file.seek(0)
+        file_content = file_content.replace(original_string, replacement_string)
+        file.write(file_content)
+        logger.info("after replacing hosts {0}".format(file_content))
 
 
 
