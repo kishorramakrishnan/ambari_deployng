d438b276227a295727aefa828313a81865dabaca
Kishor Ramakrishnan <kramakrishnan@hortonworks.com>
2017-04-04 16:26:59 -0700

    Fixing issue in replace string method

--- configs_util.py
+++ configs_util.py
@@ -27,7 +27,8 @@ def setup_ranger_ha(cluster_name,ranger_admin_hosts,load_balacer_host,ranger_adm
 def update_http_conf(cluster_name,balancer_hosts,ranger_admin_port,load_balancer_host):
     ssh_utils.run_ssh_cmd("root",load_balancer_host,"echo -e \"LoadModule ssl_module modules/mod_ssl.so\" >> /etc/httpd/conf/httpd.conf")
     final_conf_file = "conf/ranger_loadbalancer_final.conf"
-    ssh_utils.copy_file("conf/ranger_loadbalancer.conf",final_conf_file)
+    final_temp_file = "conf/ranger_loadbalancer_final.conf"
+    ssh_utils.copy_file("conf/ranger_loadbalancer.conf",final_temp_file)
     replace_string_in_file(final_conf_file,"RANGER_ADMIN_PORT_PH",ranger_admin_port)
     replace_string_in_file(final_conf_file, "CLUSTER_NAME_PH", cluster_name)
     balancer_host_string = ""
@@ -44,10 +45,15 @@ def update_http_conf(cluster_name,balancer_hosts,ranger_admin_port,load_balancer
     ssh_utils.run_ssh_cmd("root", load_balancer_host, "service httpd restart")
 
 def replace_string_in_file(file_name,original_string,replacement_string):
-    replace_cmd = "sed -i 's#{0}#{1}#g' {2}".format(original_string,replacement_string,file_name)
-    logger.info("Executing command {0}".format(replace_cmd))
-    response = ssh_utils.run_shell_command(replace_cmd)
-    logger.info("Executing command {0} COMPLETED {1}".format(replace_cmd,response[0]))
+    # Replace the file content
+    with open(file_name, 'r+') as file:
+        file_content = file.read()
+    file.seek(0)
+    file_content = file_content.replace(original_string, replacement_string)
+    file.write(file_content)
+    logger.info("after replacing hosts {0}".format(file_content))
+
+
 
 
 
